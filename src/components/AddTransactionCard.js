056072c25a608ca39477cff173a449ff62edc69f
Dimitrije Gladovic <rdomiran@gmail.com>
2023-08-26 16:36:28 +0200

    add transaction manually component

---
+++ src/components/AddTransactionCard.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { Box, Card, Typography } from '@mui/material';
+import { alpha, styled } from '@mui/material/styles';
+// utils
+// components
+import Iconify from './iconify/Iconify';
+
+
+
+export default function AddTransactionCard({ total, sx, color = 'primary', ...other }) {
+
+    const navigate = useNavigate();
+
+    const StyledIcon = styled('div')(({ theme }) => ({
+        margin: 'auto',
+        display: 'flex',
+        borderRadius: '50%',
+        alignItems: 'center',
+        width: theme.spacing(8),
+        height: theme.spacing(8),
+        justifyContent: 'center',
+        marginBottom: theme.spacing(3),
+    }));
+
+    return (
+        <>
+            <Card
+                sx={{
+                    py: 5,
+                    boxShadow: 0,
+                    textAlign: 'center',
+                    color: 'white',
+                    bgcolor: '#008faf',
+                    ...sx,
+                }}
+                {...other}
+                onClick={()=>navigate('/dashboard/scanpage')}
+            >
+                <StyledIcon
+                    sx={{
+                        color: 'white',
+                        backgroundImage: (theme) =>
+                            `linear-gradient(135deg, ${alpha('#FFFFFF', 0.65)} 0%, ${alpha(
+                                '#FFFFFF',
+                                0.24
+                            )} 100%)`,
+                    }}
+                >
+                    <Iconify icon='bi:bookmark-plus' width={24} height={24} />
+                </StyledIcon>
+
+                <Typography variant="h3">Add Transaction</Typography>
+
+                <Typography variant="subtitle2" sx={{ opacity: 0.72, paddingX:'5px' }}>
+                    Add a transaction by manualy entering transaction data.
+                </Typography>
+
+            </Card>
+        </>
+    );
+}
