7d158d6fc1b3a9193bf158c9aafde81b24a7a75a
Dimitrije Gladovic <rdomiran@gmail.com>
2023-08-29 01:56:08 +0200

    error validation on second step in parent component

--- src/components/transaction-step/TransactionStep.js
+++ src/components/transaction-step/TransactionStep.js
@@ -30,6 +30,7 @@ export default function TransactionStep() {
   const [activeStep, setActiveStep] = useState(0);
   const [template, setTemplate] = useState([]);
   const [content, setContent] = useState([]);
+  const [errorContent, setErrorContent] = useState(false);
 
   const setTemplateContext = (data) =>{
     setTemplate(data);
@@ -40,6 +41,27 @@ export default function TransactionStep() {
   }
 
   const handleNext = () => {
+    if(activeStep === 1){ // proverava da li je popunjena forma
+      const keyz = Object.values(content);
+      let validation;
+      let number = 100;
+      const valid = keyz.forEach((e,i)=>{
+        console.log(e.length,'testiram-stvari');
+        if(e.length < 2){
+          validation = false;
+          number = i;
+          return false;
+        }
+        return true;  
+      });
+      if(keyz.length === 0 || !validation){
+        console.log('testiram-nikako',number); // ovo radi
+        setErrorContent(number);
+        // potrebno je da se setuje error bool to moze u state
+      }
+        console.log(keyz,'testiram');
+        console.log(validation,'testiram2');
+    }
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
@@ -62,22 +84,23 @@ export default function TransactionStep() {
             <CardContent style={{paddingTop:'10px'}}>
                 <StandardTransaction template={template} content={content}/>
                 {activeStep === 0 && <ZeroStep templates={templates} setTemplateContext={setTemplateContext}/> }
-                {activeStep === 1 && <StartStep setTransactionContent={setTransactionContent}/>}
+                {activeStep === 1 && <StartStep setTransactionContent={setTransactionContent} error={errorContent}/>}
                 {activeStep === 2 && <LastStep/>}
             </CardContent>
             <CardActions sx={{ justifyContent: 'space-around', marginBottom:'20px' }}>
               {activeStep !== 0 && (
-                <Button onClick={handleBack} sx={{ mr: 2 }}>
+                <Button onClick={handleBack} sx={{ mr: 2, color:'black' }}>
                   Back
                 </Button>
+                // kad se stisne back obrisu se sve vrednosti iz forme to treba da se resi
               )}
               {activeStep !== steps.length - 1 && (
-                <Button onClick={handleNext} variant="contained" color="primary">
+                <Button onClick={handleNext} variant="contained" style={{backgroundColor: '#00af78'}}>
                   Next
                 </Button>
               )}
               {activeStep === steps.length - 1 && (
-                <Button variant="contained" color="primary">
+                <Button variant="contained" style={{backgroundColor: '#00af78'}}>
                   Submit
                 </Button>
               )}
