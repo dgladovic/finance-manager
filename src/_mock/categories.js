81bdd26663a1d792421ee75fd4712489c085b302
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2023-08-31 16:26:32 +0200

    added categories mock component

---
+++ src/_mock/categories.js
@@ -0,0 +1,79 @@
+// ----------------------------------------------------------------------
+
+const categories = [
+  {
+    "id": 100,
+    "name": "Hrana i Pice",
+    "color": "#f44336",
+    "iconName": "material-symbols:restaurant"
+  },
+  {
+    "id": 200,
+    "name": "Shoping",
+    "color": "#9c27b0",
+    "iconName": "mdi:cart"
+  },
+  {
+    "id": 300,
+    "name": "Kucni troskovi",
+    "color": "#2196f3",
+    "iconName": "ri:home-heart-fill"
+  },
+  {
+    "id": 400,
+    "name": "Transport",
+    "color": "#4caf50",
+    "iconName": "ic:twotone-directions-bus-filled"
+  },
+  {
+    "id": 500,
+    "name": "Vozilo",
+    "color": "#ff9800",
+    "iconName": "fluent:vehicle-car-profile-16-filled"
+  },
+  {
+    "id": 600,
+    "name": "Lifestyle i entertainment",
+    "color": "#e91e63",
+    "iconName": "fa6-solid:masks-theater"
+  },
+  {
+    "id": 700,
+    "name": "Komunikacija",
+    "color": "#795548",
+    "iconName": "teenyicons:computer-outline"
+  },
+  {
+    "id": 800,
+    "name": "Finansijski troskovi",
+    "color": "#607d8b",
+    "iconName": "ph:money-fill"
+  },
+  {
+    "id": 900,
+    "name": "Investicije",
+    "color": "#9e9e9e",
+    "iconName": "material-symbols:trending-up"
+  },
+  {
+    "id": 1000,
+    "name": "Prihod",
+    "color": "#ffeb3b",
+    "iconName": "ant-design:dollar-twotone"
+  },
+  {
+    "id": 1100,
+    "name": "Ostalo",
+    "color": "#8bc34a",
+    "iconName": "tabler:category-filled"
+  },
+  {
+    "id": 1200,
+    "name": "Nepoznato",
+    "color": "#673ab7",
+    "iconName": "material-symbols:help-outline"
+  }
+]
+
+
+export default categories;
